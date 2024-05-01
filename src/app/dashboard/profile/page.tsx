1e2d24df11da1162b7b761829a099d61653a5550
Armando Martinez <alemar.martinez16@gmail.com>
2024-05-01 13:06:03 -0300

    feat(ProfilePage): Add sign-out button and improve layout The ProfilePage component now includes a button for users to sign out, enhancing user experience by providing an easy way to log out. Additionally, the layout has been improved with better styling and organization.

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,25 +1,36 @@
-"use client"
-
-import { useSession } from "next-auth/react"
+"use client";
+import { useSession, signOut } from "next-auth/react";
 
 function ProfilePage() {
-  const { data: session, status } = useSession()
+  const { data: session, status } = useSession();
 
-  console.log({ session, status })
+  console.log(session, status);
+
   return (
-    <div>
-      <h1>Profile Page</h1>
-  
-      <pre>{JSON.stringify(
-        {
-        session,
-        status,
-        }, null , 2
-      )}</pre>
-    
+    <div className="h-[calc(100vh-4rem)] flex flex-col gap-y-10 items-center justify-center">
+      <h1 className="font-bold text-3xl">Profile</h1>
 
+      <pre className="bg-zinc-800 p-4">
+        {JSON.stringify(
+          {
+            session,
+            status,
+          },
+          null,
+          2
+        )}
+      </pre>
+
+      <button
+        className="bg-zinc-800 px-4 py-2 block mb-2"
+        onClick={() => {
+          signOut();
+        }}
+      >
+        Signout
+      </button>
     </div>
-  )
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
